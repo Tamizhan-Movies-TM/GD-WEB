9456ae196e6bcc2ca2bc3cb402cdc68838cefdf4
Karthick36 <140898906+Karthick36@users.noreply.github.com>
2025-06-14 20:26:51 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1469,7 +1469,7 @@ function file_video(name, encoded_name, size, poster, url, mimeType, file_id, co
           <span class="sr-only"></span>
           </button>
           <div class="dropdown-menu">
-					<a class="dropdown-item" href="intent://${url}#Intent;package=com.mxtech.videoplayer.ad;S.title=${encoded_name};end">MX Player (Free)</a>
+					<a class="dropdown-item" href="intent:${url}#Intent;package=com.mxtech.videoplayer.ad;end">MX Player (Free)</a>
           <a class="dropdown-item" href="intent:${url}#Intent;component=idm.internet.download.manager/idm.internet.download.manager.Downloader;S.title=${encoded_name};end">1DM (Free)</a>
           <a class="dropdown-item" href="intent:${url}#Intent;component=idm.internet.download.manager.adm.lite/idm.internet.download.manager.Downloader;S.title=${encoded_name};end">1DM (Lite)</a>
           <a class="dropdown-item" href="intent:${url}#Intent;component=idm.internet.download.manager.plus/idm.internet.download.manager.Downloader;S.title=${encoded_name};end">1DM+ (Plus)</a>
@@ -1594,7 +1594,7 @@ var content = `
           <span class="sr-only"></span>
           </button>
           <div class="dropdown-menu">
-					<a class="dropdown-item" href="intent://${url}#Intent;package=com.mxtech.videoplayer.ad;S.title=${encoded_name};end">MX Player (Free)</a>
+					<a class="dropdown-item" href="intent:${url}#Intent;package=com.mxtech.videoplayer.ad;end">MX Player (Free)</a>
           <a class="dropdown-item" href="intent:${url}#Intent;component=idm.internet.download.manager/idm.internet.download.manager.Downloader;S.title=${encoded_name};end">1DM (Free)</a>
           <a class="dropdown-item" href="intent:${url}#Intent;component=idm.internet.download.manager.adm.lite/idm.internet.download.manager.Downloader;S.title=${encoded_name};end">1DM (Lite)</a>
           <a class="dropdown-item" href="intent:${url}#Intent;component=idm.internet.download.manager.plus/idm.internet.download.manager.Downloader;S.title=${encoded_name};end">1DM+ (Plus)</a>
