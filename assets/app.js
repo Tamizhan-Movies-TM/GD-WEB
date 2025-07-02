1f5f31c9ce81ca0301083442e79122797848e2ab
Karthick36 <140898906+Karthick36@users.noreply.github.com>
2025-07-02 11:53:22 +0530

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1748,9 +1748,7 @@ function file_code(name, encoded_name, size, bytes, poster, url, mimeType, md5Ch
 	// Add the container and card elements
 	var content = `
 <div class="card">
-<div class="card-header ${UI.file_view_alert_class}">
- <i class="fas fa-file-alt fa-fw"></i>File Information
- </div>
+<div class="card-header text-truncate<i class="fas fa-file-alt fa-fw"></i>File Information ${UI.file_view_alert_class}"></div>
 	<div class="card-body">
 		<div class="row g-3">
 			<div class="col-lg-4 col-md-12 d-flex flex-column justify-content-center">  
