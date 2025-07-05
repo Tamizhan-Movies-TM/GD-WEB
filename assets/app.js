3669541b686e86a4d64ae0e86555aa99d40c1356
Karthick36 <140898906+Karthick36@users.noreply.github.com>
2025-07-05 18:04:12 +0530

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -17,7 +17,7 @@ style.textContent = `
         padding: 8px 16px;
         background: transparent; /* Fully transparent background */
         width: 160px;
-        margin-bottom: 10px;
+        margin-bottom: 5px;
         color: white !important; /* Always white text */
         display: flex;
         justify-content: center;
