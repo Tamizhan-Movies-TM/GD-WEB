cb591642780405af2908302d43a7ef757f87a046
Karthick36 <140898906+Karthick36@users.noreply.github.com>
2025-07-05 15:28:02 +0530

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1836,7 +1836,7 @@ var content = `
 						 <!-- DOWNLOAD BUTTON -->
             <div class="d-flex justify-content-center gap-3 mb-3">
             <button id="download-btn" class="glow-btn glow-secondary" btn-lg fw-bold d-flex align-items-center justify-content-center gap-2" 
-            style="width: 170px; padding: 10px 24px; font-size: 1rem; position: relative;">
+            style="width: 200px; padding: 8px 35px; font-size: 1rem; position: relative;">
             ${new_download_icon}&nbsp;&nbsp;DOWNLOAD
             <div id="download-spinner" class="spinner" style="display: none;">
             <div class="spinner-circle"></div>
