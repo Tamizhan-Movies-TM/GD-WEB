d20fe5a3e0ed7e5154b52fce65aba79cd8ad7da1
Karthick36 <140898906+Karthick36@users.noreply.github.com>
2025-07-05 21:50:27 +0530

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -103,8 +103,7 @@ document.head.appendChild(style);
 </header>
 <div class="loading" id="spinner" style="display:none;">Loading&#8230;</div>
 <div class="container" style="margin-top: ${UI.header_padding}px; margin-bottom: 60px;">
-	<div class="row align-items-start g-3">
-    </div>
+	<div class="row align-items-start g-2">
 		`+tamizhanWidget;
 		html += ` 
     <div id="content" style="${UI.fixed_footer ? 'padding-bottom: clamp(170px, 100%, 300px);' : ''}"></div>
