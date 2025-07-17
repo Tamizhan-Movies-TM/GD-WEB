52cd435dea474db3df3dfe420746b09a8afb63fd
Karthick36 <140898906+Karthick36@users.noreply.github.com>
2025-07-17 14:26:55 +0530

    Update app.js

--- temp/app.js
+++ temp/app.js
@@ -114,23 +114,23 @@ document.head.appendChild(style);
 				<div class="card-header">
 					<i class="fa-solid fa-hand-holding-dollar"></i> Pay ₹5
 				 </div>
-          <div class="card-body d-flex align-items-center justify-content-center">
-					<p class="mb-3">Pay ₹5 and send the payment screenshot to <a href="${UI.contact_link}" title="Contact Admin.">Contact Admin</a>. You'll receive a direct link for your requested movie ✅ (no ads Links)</p>
-					<div class="donate btn p-0">
+          <div class="card-body">
+					<p class="mb-2">Pay ₹5 and send the payment screenshot to <a href="${UI.contact_link}" title="Contact Admin.">Contact Admin</a>. You'll receive a direct link for your requested movie ✅ (no ads Links)</p>
+					<div class="d-flex justify-content-center">
 						<a class="btn" href="https://tamizhan-movies.unaux.com" title="Click me!" style="background: #3B556E;" target="_blank">
 						<i class="fa-solid fa-money-check-dollar"></i> Click Here To Payment</a>
-						<div class="qrcode card" style="padding: 1rem 1rem 0 1rem;">
-							<div style="padding-bottom: 1rem;">Thank you very much ❤</div>
-							<img alt="Love" src="https://kaceku.onrender.com/static/img/love.png">
-						   </div>
-					    </div>
+					</div>
+					<div class="qrcode card text-center mt-3" style="padding: 1rem 1rem 0 1rem;">
+						<div style="padding-bottom: 1rem;">Thank you very much ❤</div>
+						<img alt="Love" src="https://kaceku.onrender.com/static/img/love.png">
+					</div>
              </div>
         	 </div>
           </div>
            <div class="col-lg-6 col-md-12">
           	<div class="card text-white mb-3 h-100">
              <div class="card-header">
-             <i class="fa-brands fa-telegram fa-fw"></i>&nbsp;&nbsp;Join &nbsp;Our &nbsp;Telegram &nbsp;Channels
+             <i class="fa-brands fa-telegram fa-fw"></i>&nbsp;&nbsp;Join Our Telegram Channels
             </div>
             <div class="card-body d-flex flex-wrap gap-2 justify-content-evenly align-items-center">
 					<a href="https://telegram.me/+p9abEnd3wHAxZjQ9" target="_blank" title="tamizahan_Movies">
@@ -144,6 +144,7 @@ document.head.appendChild(style);
 					</a>
 					<a href="https://telegram.me/+4ZuXoVoy8841MjM1" target="_blank" title="telegram">
 						<img class="image" alt="telegram" style="height: 50px;" src="https://i.ibb.co/84TDPHg1/ef456f9f9052b4e8821c20a22f377838.png">
+					</a>
           </div> 
           </div>
         </div>
