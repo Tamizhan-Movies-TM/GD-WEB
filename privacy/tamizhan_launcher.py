0154c81025c098ce4a3ef5d5f652ebe707f70e1b
Tamizhan-Movies-TM <powermango33@gmail.com>
2026-05-04 00:10:18 +0530

    Update tamizhan_launcher.py

--- privacy/tamizhan_launcher.py
+++ privacy/tamizhan_launcher.py
@@ -9,7 +9,7 @@ print("✅ Auth & Drive ready!")
 
 # ── Step 2: Download & Run Script ────────────────────────────────────
 url = "https://cdn.jsdelivr.net/gh/Tamizhan-Movies-TM/GD-WEB@master/privacy/tamizhan_v8_7.py"
-print(f"⬇ Loading script...")
+print(f"🏃🏻‍♀️‍➡️ Runing script...")
 code = urllib.request.urlopen(url).read().decode()
-print("🚀 Starting Tamizhan v8.6...\n")
+print("🚀 Starting Tamizhan v8.7...\n")
 exec(code, {"__name__": "__main__"})
